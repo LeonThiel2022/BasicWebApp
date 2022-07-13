7617ba0a046b77aaf688e4393e334c0c0c1d6898
ge78rux <Leon.H.Thiel@web.de>
2022-07-13 10:52:50 +0200

    test

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
+
 		query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
